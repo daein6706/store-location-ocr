57a459c45afd35fc8bebe592d1c672f6a9f88abd
daein6706 <md.daein@mediquitous.com>
2026-03-06 10:56:11 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "location-app-v2";
+const CACHE_NAME = "location-app-v3";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
@@ -53,4 +53,5 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
